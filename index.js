7996fcdda680376099ade49291a3201669cb4ec4
Magda <magdap.265@gmail.com>
2019-06-24 14:17:02 +0200

    pobiera wybrane dane (nazwę i grupę) z api i zapisuje je w tabeli wyświetlanej na tronie

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-fetch("http://lrodak:3001/api/users")
-    .then((resp) => resp.json()) // Transform the data into json
-    .then(function(data) {
-        var userTable = document.getElementById("usersTable");
-        var row = userTable.insertRow();
-        var cell0 = row.insertCell(0);
-        var cell1 = row.insertCell(1);
+// fetch("http://lrodak:3001/api/users")
+//     .then((resp) => resp.json()) // Transform the data into json
+//     .then(function(data){ 
+         let data=[
+             {name:"imie",group:{name:"administratorzy"}},
+             {name:"imie1", group:{name:"administratorzy"}}];
+        let userTable = document.getElementById("usersTable");
+        let row = userTable.insertRow();
+        let cell0 = row.insertCell(0);
+        let cell1 = row.insertCell(1);
         cell0.innerHTML = "Nazwa";
         cell1.innerHTML = "Grupa";
-        for (var i = 0; i < data.length; i++) {
-            var rowData = userTable.insertRow();
-            var cell0 = rowData.insertCell(0);
-            var cell1 = rowData.insertCell(1);
-            cell0.innerHTML = data[i].name;
-            cell1.innerHTML = data[i].group.name;
+        for (const user of data) {
+            let rowData = userTable.insertRow();
+            let cell0 = rowData.insertCell(0);
+            let cell1 = rowData.insertCell(1);
+            cell0.innerHTML = user.name;
+            cell1.innerHTML = user.group.name;
         }
-    });
+        //for (let i = 0; i < data.length; i++) {        
+        //     let rowData = userTable.insertRow();
+        //     let cell0 = rowData.insertCell(0);
+        //     let cell1 = rowData.insertCell(1);
+        //     cell0.innerHTML = data[i].name;
+        //     cell1.innerHTML = data[i].group.name;
+        // }
+    // });
